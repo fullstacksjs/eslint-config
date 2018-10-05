d4d6af81a879e038f24879357df6e89e9d2adf21
frontendmonster <frontendmonster@gmail.com>
2018-10-06 02:53:11 +0330

    Add react/prop-types

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ module.exports = {
     "react/destructuring-assignment": 0,
     "react/jsx-filename-extension": 0,
     "react/jsx-one-expression-per-line": 0,
+    "react/prop-types": [1, { "ignore": ["children", "className"] }],
     "react/sort-comp": 0
   },
   "overrides": [
