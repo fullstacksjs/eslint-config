0adaa9ddf4062e3d3a410e7d0c9fcea321114969
frontendmonster <frontendmonster@gmail.com>
2021-03-30 11:14:50 +0430

    fp: move some rules to strict mode
    * fp/no-mutating-methods: move to strict mode
    * fp/no-mutation: move to strict mode

--- rules/strict.js
+++ rules/strict.js
@@ -3,6 +3,8 @@ module.exports = {
     'no-console': 'warn',
     'fp/no-delete': 'error',
     'fp/no-get-set': 'error',
+    'fp/no-mutating-methods': 'error',
+    'fp/no-mutation': ['error', { commonjs: true, allowThis: true, exceptions: [{ property: 'propTypes' }, { property: 'defaultProps' }] }],
     'fp/no-nil': 'error',
     'fp/no-this': 'error',
     'fp/no-throw': 'error',
