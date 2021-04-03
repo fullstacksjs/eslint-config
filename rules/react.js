7844223461c75565cbe319e3ba415b81524404a3
frontendmonster <frontendmonster@gmail.com>
2021-04-03 16:48:59 +0430

    react/react-in-jsx-scope: off with automatic jsx runtime

--- rules/react.js
+++ rules/react.js
@@ -67,7 +67,7 @@ module.exports = {
     'react/prefer-read-only-props': 'off',
     'react/prefer-stateless-function': 'off',
     'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'error',
+    'react/react-in-jsx-scope': 'off', // jsx automatic runtime
     'react/require-default-props': 'off',
     'react/require-optimization': 'off',
     'react/require-render-return': 'error',
