b2a96c4cd0a4b6278495d9bb0ad5548f57a48da1
Alireza Safaierad <frontendmonster@gmail.com>
2022-04-24 13:27:28 +0400

    fix: improve nextjs config

--- nextjs.js
+++ nextjs.js
@@ -1,3 +1,13 @@
+const fs = require('fs');
+const { Packages } = require('@frontendmonster/utils');
+const readPkgUp = require('read-pkg-up');
+
+const { packageJson: pkg } = readPkgUp.sync({
+  cwd: fs.realpathSync(process.cwd()),
+});
+
+const packages = Packages(pkg);
+
 /** @type { import('eslint').Linter.Config } */
 module.exports = {
   plugins: ['prettier', 'simple-import-sort', 'promise', 'node', 'fp'],
@@ -12,6 +22,11 @@ module.exports = {
     node: true,
   },
   extends: [
+    require.resolve('./storybook'),
+    packages.ifAnyDep('jest', () => require.resolve('./jest')),
+    packages.ifAnyDep('react', () => require.resolve('./react')),
+    packages.ifAnyDep('typescript', () => require.resolve('./typescript')),
+    packages.ifAnyDep('cypress', () => require.resolve('./cypress')),
     './rules/base',
     './rules/es2015',
     './rules/forbidden',
