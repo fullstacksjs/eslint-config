c00752314b2748664f483920a9989d220cfff9bb
Alireza Safaierad <frontendmonster@gmail.com>
2023-07-27 23:10:04 +0400

    fix: fix react-hooks version conlict

--- next.js
+++ next.js
@@ -1,6 +1,6 @@
 /** @type { import('eslint').Linter.Config } */
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: ['plugin:@next/next/core-web-vitals'],
   rules: {
     'react/function-component-definition': 'off',
   },
