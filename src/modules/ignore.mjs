f8b0e36a6c9be19b001b27d918425cb6cae6251f
Alireza Safaierad <frontendmonster@gmail.com>
2025-03-21 14:33:30 +0100

    fix: fix ignore config

--- src/modules/ignore.mjs
+++ src/modules/ignore.mjs
@@ -1,12 +1,12 @@
+import { globalIgnores } from 'eslint/config';
+
 import { ignoreGlobs } from '../utils/globs.mjs';
 /**
  * @param { import('../option').Options } options
  * @return { import('eslint').Linter.Config }
  */
 function ignores(options = {}) {
-  return {
-    ignores: [...ignoreGlobs, ...(options.ignores ?? [])],
-  };
+  return globalIgnores([...ignoreGlobs, ...options.ignores]);
 }
 
 export default ignores;
