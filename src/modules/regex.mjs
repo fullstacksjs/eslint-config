b7f18178c4b7a5af3b34f602620f70a477e01e2f
Amirreza Rahmani <amirbabble@gmail.com>
2025-07-03 03:29:31 +0330

    feat: add regex plugin

---
+++ src/modules/regex.mjs
@@ -0,0 +1,87 @@
+import plugin from 'eslint-plugin-regexp';
+
+/**
+ * @param { import('..').Options } options
+ * @return { Promise<import('eslint').Linter.Config> }
+ */
+function regex(options = {}) {
+  return {
+    plugins: { regexp: plugin },
+    settings: {
+      regexp: {
+        allowedCharacterRanges: options.regex.allowedCharacterRanges ?? ['all'],
+      },
+    },
+    rules: {
+      'regexp/confusing-quantifier': 'warn',
+      'regexp/control-character-escape': 'error',
+      'regexp/letter-case': 'warn',
+      'regexp/match-any': 'error',
+      'regexp/negation': 'error',
+
+      'regexp/no-contradiction-with-assertion': 'error',
+      'regexp/no-dupe-characters-character-class': 'error',
+      'regexp/no-dupe-disjunctions': 'error',
+      'regexp/no-empty-alternative': 'warn',
+      'regexp/no-empty-capturing-group': 'error',
+      'regexp/no-empty-character-class': 'error',
+      'regexp/no-empty-group': 'error',
+      'regexp/no-empty-lookarounds-assertion': 'error',
+      'regexp/no-empty-string-literal': 'error',
+      'regexp/no-escape-backspace': 'error',
+      'regexp/no-extra-lookaround-assertions': 'error',
+      'regexp/no-invalid-regexp': 'error',
+      'regexp/no-invisible-character': 'error',
+      'regexp/no-lazy-ends': 'warn',
+      'regexp/no-legacy-features': 'error',
+      'regexp/no-misleading-capturing-group': 'error',
+      'regexp/no-misleading-unicode-character': 'error',
+      'regexp/no-missing-g-flag': 'error',
+      'regexp/no-non-standard-flag': 'error',
+      'regexp/no-obscure-range': 'error',
+      'regexp/no-optional-assertion': 'error',
+      'regexp/no-potentially-useless-backreference': 'warn',
+      'regexp/no-super-linear-backtracking': 'error',
+      'regexp/no-trivially-nested-assertion': 'error',
+      'regexp/no-trivially-nested-quantifier': 'error',
+      'regexp/no-unused-capturing-group': 'error',
+      'regexp/no-useless-assertions': 'error',
+      'regexp/no-useless-backreference': 'error',
+      'regexp/no-useless-character-class': 'error',
+      'regexp/no-useless-dollar-replacements': 'error',
+      'regexp/no-useless-escape': 'error',
+      'regexp/no-useless-flag': 'warn',
+      'regexp/no-useless-lazy': 'error',
+      'regexp/no-useless-non-capturing-group': 'error',
+      'regexp/no-useless-quantifier': 'error',
+      'regexp/no-useless-range': 'error',
+      'regexp/no-useless-set-operand': 'error',
+      'regexp/no-useless-string-literal': 'error',
+      'regexp/no-useless-two-nums-quantifier': 'error',
+      'regexp/no-zero-quantifier': 'error',
+
+      'regexp/optimal-lookaround-quantifier': 'warn',
+      'regexp/optimal-quantifier-concatenation': 'error',
+
+      'regexp/prefer-character-class': 'error',
+      'regexp/prefer-d': 'error',
+      'regexp/prefer-plus-quantifier': 'error',
+      'regexp/prefer-predefined-assertion': 'error',
+      'regexp/prefer-question-quantifier': 'error',
+      'regexp/prefer-range': 'error',
+      'regexp/prefer-set-operation': 'error',
+      'regexp/prefer-star-quantifier': 'error',
+      'regexp/prefer-unicode-codepoint-escapes': 'error',
+      'regexp/prefer-w': 'error',
+      'regexp/simplify-set-operations': 'error',
+      'regexp/strict': 'error',
+      'regexp/use-ignore-case': 'error',
+
+      'regexp/sort-flags': 'warn',
+      'regexp/sort-alternatives': 'warn',
+      'regexp/sort-character-class-elements': 'warn',
+    },
+  };
+}
+
+export default regex;
