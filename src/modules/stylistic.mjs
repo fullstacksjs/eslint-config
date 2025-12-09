228371129db44b128ef5e733b9f477983f4f4509
MohammadJavad Bagheri <javadbagheri25452545@gmail.com>
2025-12-09 21:33:43 +0330

    feat(stylistic): add new rules

--- src/modules/stylistic.mjs
+++ src/modules/stylistic.mjs
@@ -9,6 +9,31 @@ function stylistic() {
     plugins: { '@stylistic': stylisticPlugin },
     rules: {
       '@stylistic/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never', propElementValues: 'always' }],
+      'padding-line-between-statements': [
+        'warn',
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+        { blankLine: 'always', prev: ['case', 'default'], next: '*' },
+        { blankLine: 'always', prev: '*', next: ['return', 'if', 'for', 'while'] },
+        { blankLine: 'always', prev: ['block-like', 'function', 'class', 'iife', 'try'], next: '*' },
+        { blankLine: 'always', prev: '*', next: ['block-like', 'function', 'class', 'iife', 'try'] },
+        { blankLine: 'always', prev: 'directive', next: '*' },
+        { blankLine: 'never', prev: 'directive', next: 'directive' },
+        { blankLine: 'always', prev: 'import', next: '*' },
+        { blankLine: 'any', prev: '*', next: 'import' },
+        { blankLine: 'always', prev: 'directive', next: 'import' },
+      ],
+      '@stylistic/jsx-self-closing-comp': [
+        'warn',
+        {
+          component: true,
+          html: true,
+        },
+      ],
     },
   };
 }
