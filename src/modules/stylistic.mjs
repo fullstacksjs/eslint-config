956e745027bfc6025554e138d2b9084e3749178b
MohammadJavad Bagheri <javadbagheri25452545@gmail.com>
2025-12-16 11:48:45 +0330

    fix(stylistic): fix padding-line-between-statements to be more general

--- src/modules/stylistic.mjs
+++ src/modules/stylistic.mjs
@@ -19,40 +19,16 @@ function stylistic() {
       '@stylistic/no-mixed-operators': 'error',
       '@stylistic/padding-line-between-statements': [
         'warn',
-        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-        {
-          blankLine: 'any',
-          prev: ['const', 'let', 'var'],
-          next: ['const', 'let', 'var'],
-        },
         { blankLine: 'always', prev: ['case', 'default'], next: '*' },
-        {
-          blankLine: 'never',
-          prev: ['if', 'for', 'while', 'function', 'class', 'iife', 'try'],
-          next: ['if', 'for', 'while', 'function', 'class', 'iife', 'try'],
-        },
-        {
-          blankLine: 'always',
-          prev: '*',
-          next: 'multiline-block-like',
-        },
-        {
-          blankLine: 'always',
-          prev: 'multiline-block-like',
-          next: '*',
-        },
+        { blankLine: 'always', prev: '*', next: 'multiline-block-like' },
+        { blankLine: 'always', prev: 'multiline-block-like', next: '*' },
         { blankLine: 'always', prev: ['type', 'interface'], next: '*' },
-        {
-          blankLine: 'any',
-          prev: ['type', 'interface'],
-          next: ['type', 'interface', 'const', 'let', 'var'],
-        },
-        { blankLine: 'always', prev: '*', next: ['return', 'throw'] },
         { blankLine: 'always', prev: 'directive', next: '*' },
         { blankLine: 'never', prev: 'directive', next: 'directive' },
         { blankLine: 'always', prev: 'import', next: '*' },
         { blankLine: 'any', prev: '*', next: 'import' },
         { blankLine: 'always', prev: 'directive', next: 'import' },
+        { blankLine: 'any', prev: ['if'], next: ['if'] },
       ],
       '@stylistic/lines-between-class-members': [
         'warn',
