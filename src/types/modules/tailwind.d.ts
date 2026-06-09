79bd81a6d0c3b8dc3cdb8c33c31803f7797973af
Amirreza <amirbabble@gmail.com>
2026-06-09 12:55:05 +0330

    fix(tailwind): validate settings

--- src/types/modules/tailwind.d.ts
+++ src/types/modules/tailwind.d.ts
@@ -1,18 +1,11 @@
 import type { ConfigWithOverrides } from '..';
 
-interface TailwindOptionsBase extends ConfigWithOverrides {
-  callees?: string[];
-  variables?: string[];
-  attributes?: string[];
-  tags?: string[];
-}
-
-interface TailwindOptionsWithEntryPoint extends TailwindOptionsBase {
+interface TailwindOptionsWithEntryPoint extends ConfigWithOverrides {
   entryPoint: string;
   tailwindConfig?: string;
 }
 
-interface TailwindOptionsWithConfig extends TailwindOptionsBase {
+interface TailwindOptionsWithConfig extends ConfigWithOverrides {
   entryPoint?: string;
   tailwindConfig: string;
 }
