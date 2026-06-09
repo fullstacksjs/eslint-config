daad062329927e6e14551b0def06fa6d3ee994d9
Amirreza <amirbabble@gmail.com>
2026-06-09 14:29:56 +0330

    feat(node): disable no-unpublished-import

--- src/modules/node.mjs
+++ src/modules/node.mjs
@@ -33,7 +33,7 @@ function node(options = {}) {
       'n/no-sync': strict(options, 'warn'),
       'n/no-top-level-await': 'off',
       'n/no-unpublished-bin': 'warn',
-      'n/no-unpublished-import': 'warn',
+      'n/no-unpublished-import': 'off',
       'n/no-unpublished-require': 'warn',
       'n/no-unsupported-features/es-builtins': 'off', // Nice but not a general rule
       'n/no-unsupported-features/es-syntax': 'off', // Nice but not a general rule
